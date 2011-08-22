89ef4eb18e4b5977d049619e0f349dcb4933d370
Jonathan Martens <jonathan@snetram.nl>
2011-08-22 15:23:30 +0200

    Fix a minor typo

--- build/build.sh
+++ build/build.sh
@@ -2,4 +2,4 @@
 wget https://raw.github.com/seaofclouds/tweet/master/tweet/jquery.tweet.js -O js/jquery.tweet.js
 wget https://raw.github.com/seaofclouds/tweet/master/tweet/jquery.tweet.css -O css/jquery.tweet.css
 cd ../
-tar -czf build/mod_snetram_tweet.tgz index.html mod_snetram_tweet.xml mod_snetram_tweet.php helper.php css/ js/ lnaguage/ tmpl/
+tar -czf build/mod_snetram_tweet.tgz index.html mod_snetram_tweet.xml mod_snetram_tweet.php helper.php css/ js/ language/ tmpl/
