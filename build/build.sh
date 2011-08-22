c3a99cda9aff5ef0494aeb4a49cc9159fb733573
Jonathan Martens <jonathan@snetram.nl>
2011-08-22 23:17:06 +0200

    Update build script to include license file in archive

--- build/build.sh
+++ build/build.sh
@@ -2,5 +2,5 @@
 wget https://raw.github.com/seaofclouds/tweet/master/tweet/jquery.tweet.js -O js/jquery.tweet.js
 wget https://raw.github.com/seaofclouds/tweet/master/tweet/jquery.tweet.css -O css/jquery.tweet.css
 cd ../
-tar -czf build/mod_snetram_tweet.tgz index.html mod_snetram_tweet.xml mod_snetram_tweet.php helper.php css/ js/ language/ tmpl/
+tar -czf build/mod_snetram_tweet.tgz gpl-3.0.txt index.html mod_snetram_tweet.xml mod_snetram_tweet.php helper.php css/ js/ language/ tmpl/
 tar tvzf build/mod_snetram_tweet.tgz
