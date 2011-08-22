61644e4abcb082f782347771f0ab8e3eaf77d1bb
Jonathan Martens <jonathan@snetram.nl>
2011-08-22 15:26:06 +0200

    List the contents of the created archive... just to make sure

--- build/build.sh
+++ build/build.sh
@@ -3,3 +3,4 @@ wget https://raw.github.com/seaofclouds/tweet/master/tweet/jquery.tweet.js -O js
 wget https://raw.github.com/seaofclouds/tweet/master/tweet/jquery.tweet.css -O css/jquery.tweet.css
 cd ../
 tar -czf build/mod_snetram_tweet.tgz index.html mod_snetram_tweet.xml mod_snetram_tweet.php helper.php css/ js/ language/ tmpl/
+tar tvzf build/mod_snetram_tweet.tgz
